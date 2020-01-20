53843615ffbc2f438bd4e16d350ed7a272e6f691
Santiago Del Salto <santyrdse@gmail.com>
2020-01-20 02:56:32 -0500

    Files related with iOS

---
+++ SuggestionESPE/SuggestionESPE/SuggestionESPE.iOS/IosForceKeyboardDismissalService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using SuggestionESPE.Event;
+
+namespace SuggestionESPE.iOS
+{
+    public class IosForceKeyboardDismissalService : ForceDismissKeyboard
+    {
+        public void DismissKeyboard()
+        {
+            UIApplication.SharedApplication.InvokeOnMainThread(() =>
+            {
+                var window = UIApplication.SharedApplication.KeyWindow;
+                var vc = window.RootViewController;
+                while (vc.PresentedViewController != null)
+                {
+                    vc = vc.PresentedViewController;
+                }
+
+                vc.View.EndEditing(true);
+            });
+
+        }
+    }
+}
