6f8afa3d31f385939f75a4b92f7e9f7675a78e88
Santiago Del Salto <srdel@espe.edu.ec>
2020-01-28 23:43:57 -0500

    Added some API Consumption

---
+++ SuggestionPortalESPE/SuggestionPortalESPE/SuggestionPortalESPE/Models/PublicationModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuggestionPortalESPE.Models
+{
+    public class PublicationModel
+    {
+        public int Lepub_Id { get; set; }
+        public int Lepub_Anonymous { get; set; }
+        public int Lepub_Complaint { get; set; }
+        public String Lepub_Message { get; set; }
+        public String Lepub_Type { get; set; }
+        public int Letop_Id { get; set; }
+        public String Leuser_IdEspe { get; set; }
+
+    }
+}
